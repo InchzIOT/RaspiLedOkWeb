78e1585e023dbd3f7b13118042ebd7c59d222514
liewxen <82050355+liewxen@users.noreply.github.com>
2025-08-27 15:44:08 +0800

    Update web host settings for local development
    - Set web host to listen on all interfaces at port 5000.
    - Change application URL in launchSettings.json to localhost.
    - Fix typo in HTTPS URL from 'locahost' to 'localhost'.

--- RaspiLedOkWeb/Program.cs
+++ RaspiLedOkWeb/Program.cs
@@ -3,6 +3,8 @@ using RaspiLedOkWeb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
